6d20293a741f63d6cecb4bae9cc61466807c51a6
peter <jy02927731@yahoo.com.hk>
2016-10-22 20:41:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# facebook-bot-peter
+
+> This is a example of building a facebook bot
+
+> original: https://github.com/fbsamples/messenger-platform-samples/tree/master/node
+
+# Step
+### 1) set MESSAGER_APP_SECRET on setEnvVal.sh
+![MESSAGER_APP_SECRET](https://storage.googleapis.com/bundle_asia_dra/temp/temp/MESSENGER_APP_SECRET%20)
+
+### 2) set MESSAGER_APP_ACCESS_TOKEN on setEnvVal.sh
+![MESSAGER_APP_ACCESS_TOKEN](https://storage.googleapis.com/bundle_asia_dra/temp/temp/MESSENGER_PAGE_ACCESS_TOKEN.jpg)
+
+### 3) set MESSAGER_VALIDATION_TOKEN on setEnvVal.sh
+![MESSAGER_VALIDATION_TOKEN](https://storage.googleapis.com/bundle_asia_dra/temp/temp/MESSENGER_VALIDATION_TOKEN.jpg)
+
+### 4) Go to your heroku app page, set SEVER_URL on setEnvVal.sh
+![SEVER_URL](https://storage.googleapis.com/bundle_asia_dra/temp/temp/SERVER_URL.jpg)
+
+### 5) Go to your heroku app page, set all the four ENV values
+![heroku](https://storage.googleapis.com/bundle_asia_dra/temp/temp/HEROKU_ENV.jpg)
+
+
+# Run
+
+```js
+$ git clone git@github.com:wahengchang/facebook-bot-template.git
+$ cd facebook-bot-template
+$
+$ //set up your env values in setEnvSample.sh
+$ source setEnvSample.sh
+$ npm start
+$ // Node app is running on port 5000
+$
+$ // login to your heroku and set up git remote setting
+$ // don't forget to set up env values on heroku
+$ git add .
+$ git commit -m "first commit"
+$ git push heroku master
+$
+$
---
+++ app.js
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+/* jshint node: true, devel: true */
+'use strict';
+
+var bodyParser = require('body-parser'),
+    config = require('config'),
+    crypto = require('crypto'),
+    express = require('express'),
+    https = require('https'),
+    request = require('request'),
+    facebookModule = new require('./module/facebook')();
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({ verify: facebookModule.verifyRequestSignature }));
+app.use(express.static('public'));
+
+/******************************************************************************************
+Facebook env setting
+******************************************************************************************/
+
+var VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN ? process.env.MESSENGER_VALIDATION_TOKEN : config.get('validationToken');
+if (!VALIDATION_TOKEN) {
+    console.error("Missing config values");
+    process.exit(1);
+}
+
+/******************************************************************************************
+End facebook env setting
+******************************************************************************************/
+
+
+
+
+/*
+ * Use your own validation token. Check that the token used in the Webhook
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function (pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function (messagingEvent) {
+        if (messagingEvent.optin) {
+          facebookModule.receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          facebookModule.receivedMessage(messagingEvent);
+          //Message types:
+          //echo
+          //quickReply
+          //messageText
+          //messageAttachments
+        } else if (messagingEvent.delivery) {
+          facebookModule.receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          facebookModule.receivedPostback(messagingEvent);
+        } else if (messagingEvent.read) {
+          facebookModule.receivedMessageRead(messagingEvent);
+        } else if (messagingEvent.account_linking) {
+          facebookModule.receivedAccountLink(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
+});
+
+/*
+ * This path is used for account linking. The account linking call-to-action
+ * (sendAccountLinking) is pointed to this URL.
+ *
+ */
+app.get('/authorize', function (req, res) {
+  var accountLinkingToken = req.query.account_linking_token;
+  var redirectURI = req.query.redirect_uri;
+
+  // Authorization Code should be generated per user by the developer. This will
+  // be passed to the Account Linking callback.
+  var authCode = "1234567890";
+
+  // Redirect users to this URI on successful login
+  var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
+
+  res.render('authorize', {
+    accountLinkingToken: accountLinkingToken,
+    redirectURI: redirectURI,
+    redirectURISuccess: redirectURISuccess
+  });
+});
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid
+// certificate authority.
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
