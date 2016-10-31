263afcbac36fb0392517a687ca68158ca09aa7e9
peter <jy02927731@yahoo.com.hk>
2016-10-31 17:06:24 +0800

    reduce functions of Facebook.module

--- app.js
+++ app.js
@@ -44,52 +44,13 @@ app.post('/webhook', parsePOST, function (req, res) {
   console.log(' ****************  POST /webhook **********************')
   console.log(JSON.stringify(req.body))
 
-  // var data = req.body;
-
   var data = req.afterParse ;
 
-  console.log(' ************ data **************')
+  console.log(' ****************  data **********************')
   console.log(JSON.stringify(data))
 
-  // Make sure this is a page subscription
-  // if (data.object == 'page') {
-  //   // Iterate over each entry
-  //   // There may be multiple if batched
-  //   data.entry.forEach(function (pageEntry) {
-  //     var pageID = pageEntry.id;
-  //     var timeOfEvent = pageEntry.time;
 
-  //     // Iterate over each messaging event
-  //     pageEntry.messaging.forEach(function (messagingEvent) {
-  //       if (messagingEvent.optin) {
-  //         facebookModule.receivedAuthentication(messagingEvent);
-  //       } else if (messagingEvent.message) {
-  //         facebookModule.receivedMessage(messagingEvent);
-  //         //Message types:
-  //         //echo
-  //         //quickReply
-  //         //messageText
-  //         //messageAttachments
-  //       } else if (messagingEvent.delivery) {
-  //         facebookModule.receivedDeliveryConfirmation(messagingEvent);
-  //       } else if (messagingEvent.postback) {
-  //         facebookModule.receivedPostback(messagingEvent);
-  //       } else if (messagingEvent.read) {
-  //         facebookModule.receivedMessageRead(messagingEvent);
-  //       } else if (messagingEvent.account_linking) {
-  //         facebookModule.receivedAccountLink(messagingEvent);
-  //       } else {
-  //         console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-  //       }
-  //     });
-  //   });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
     res.sendStatus(200);
-  // }
 });
 
 /*
--- module/facebook/_index.js
+++ module/facebook/_index.js
@@ -80,6 +80,117 @@ function facebookModule() {
     }
 
     /*
+     * Message Event
+     *
+     * This event is called when a message is sent to your page. The 'message'
+     * object format can vary depending on the kind of message that was received.
+     * Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+     *
+     * For this example, we're going to echo any text that we get. If we get some
+     * special keywords ('button', 'generic', 'receipt'), then we'll send back
+     * examples of those bubbles to illustrate the special message bubbles we've
+     * created. If we receive a message with an attachment (image, video, audio),
+     * then we'll simply confirm that we've received the attachment.
+     *
+     */
+    function receivedMessage(event) {
+        var senderID = event.sender.id;
+        var recipientID = event.recipient.id;
+        var timeOfMessage = event.timestamp;
+        var message = event.message;
+
+        console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+        console.log(JSON.stringify(message));
+
+        var isEcho = message.is_echo;
+        var messageId = message.mid;
+        var appId = message.app_id;
+        var metadata = message.metadata;
+
+        // You may get a text or attachment but not both
+        var messageText = message.text;
+        var messageAttachments = message.attachments;
+        var quickReply = message.quick_reply;
+
+        if (isEcho) {
+            // Just logging message echoes to console
+            console.log("Received echo for message %s and app %d with metadata %s", messageId, appId, metadata);
+            return;
+        } else if (quickReply) {
+            var quickReplyPayload = quickReply.payload;
+            console.log("Quick reply for message %s with payload %s", messageId, quickReplyPayload);
+
+            sendTextMessage(senderID, "Quick reply tapped");
+            return;
+        }
+
+        if (messageText) {
+
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding example. Otherwise, just echo
+            // the text we received.
+            switch (messageText) {
+                case 'image':
+                    sendImageMessage(senderID);
+                    break;
+
+                case 'gif':
+                    sendGifMessage(senderID);
+                    break;
+
+                case 'audio':
+                    sendAudioMessage(senderID);
+                    break;
+
+                case 'video':
+                    sendVideoMessage(senderID);
+                    break;
+
+                case 'file':
+                    sendFileMessage(senderID);
+                    break;
+
+                case 'button':
+                    sendButtonMessage(senderID);
+                    break;
+
+                case 'generic':
+                    sendGenericMessage(senderID);
+                    break;
+
+                case 'receipt':
+                    sendReceiptMessage(senderID);
+                    break;
+
+                case 'quick reply':
+                    sendQuickReply(senderID);
+                    break;
+
+                case 'read receipt':
+                    sendReadReceipt(senderID);
+                    break;
+
+                case 'typing on':
+                    sendTypingOn(senderID);
+                    break;
+
+                case 'typing off':
+                    sendTypingOff(senderID);
+                    break;
+
+                case 'account linking':
+                    sendAccountLinking(senderID);
+                    break;
+
+                default:
+                    sendTextMessage(senderID, messageText);
+            }
+        } else if (messageAttachments) {
+            sendTextMessage(senderID, "Message with attachment received");
+        }
+    }
+
+    /*
      * Delivery Confirmation Event
      *
      * This event is sent to confirm the delivery of a message. Read more about
@@ -287,7 +398,7 @@ function facebookModule() {
             }
         };
 
-        return callSendAPI(messageData);
+        callSendAPI(messageData);
     }
 
     /*
@@ -557,31 +668,27 @@ function facebookModule() {
      *
      */
     function callSendAPI(messageData) {
-        return new Promise(function(resolve, reject) {
+        request({
+            uri: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: PAGE_ACCESS_TOKEN
+            },
+            method: 'POST',
+            json: messageData
 
-            request({
-                uri: 'https://graph.facebook.com/v2.6/me/messages',
-                qs: {
-                    access_token: PAGE_ACCESS_TOKEN
-                },
-                method: 'POST',
-                json: messageData
+        }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var recipientId = body.recipient_id;
+                var messageId = body.message_id;
 
-            }, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var recipientId = body.recipient_id;
-                    var messageId = body.message_id;
-
-                    resolve({
-                        messageId: messageId,
-                        recipientId: recipientId,
-                        message: "Successfully sent message with id "+ messageId + " to recipient " + recipientId
-                    });
+                if (messageId) {
+                    console.log("Successfully sent message with id %s to recipient %s", messageId, recipientId);
                 } else {
-                    console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-                    reject(error);
+                    console.log("Successfully called Send API for recipient %s", recipientId);
                 }
-            });
+            } else {
+                console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+            }
         });
     }
 
@@ -649,9 +756,19 @@ function facebookModule() {
 
     return {
         verifyRequestSignature: verifyRequestSignature,
+        receivedAuthentication: receivedAuthentication,
+        receivedMessage: receivedMessage,
+        receivedPostback: receivedPostback,
+        receivedMessageRead: receivedMessageRead,
         sendButtonMessage: sendButtonMessage,
+        sendGenericMessage: sendGenericMessage,
+        sendReceiptMessage: sendReceiptMessage,
         sendQuickReply: sendQuickReply,
+        sendReadReceipt: sendReadReceipt,
+        sendTypingOn: sendTypingOn,
+        sendTypingOff: sendTypingOff,
         sendTextMessage: sendTextMessage,
+        receivedDeliveryConfirmation: receivedDeliveryConfirmation,
         sendAccountLinking: sendAccountLinking,
         validateGET: validateGET,
         parsePOST: parsePOST
