68c10e00a73ab9788a3388c92e77393e26b1d7ad
peter <jy02927731@yahoo.com.hk>
2016-10-31 14:24:54 +0800

    comment things that is not used

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.post('/webhook', function (req, res) {
     // You must send back a 200, within 20 seconds, to let us know you've
     // successfully received the callback. Otherwise, the request will time out.
     res.sendStatus(200);
-  }
+  // }
 });
 
 /*
